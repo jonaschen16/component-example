cef3802bfe7d3236b7d9e408e82c1f4687fdf5cf
jonaschen16 <jonaschen@yahoo.com>
2023-12-07 10:35:34 +0800

    layout and comments finished

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const commentsData = [
+    { creator: 'Alex', comment: 'Great video', likedCount: 10 },
+    { creator: 'Olivia', comment: 'Thank you so much!', likedCount: 3 },
+    { creator: 'Lucas', comment: 'Awesome!', likedCount: 0 }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='columns'>
+        <div className='primary'>
+          <div className='video-player'>VideoPlayer</div>
+          <div className='comments'>
+            {
+              commentsData.map((comment) => {
+                return (
+                  <div class="comment">
+                    <h3 class="comment-header">{ comment.creator }</h3>
+                    <p class="comment-body">{ comment.comment }</p>
+                    <div class="comment-actions">
+                      <button>Like</button>
+                      <span>{ comment.likedCount }</span>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+        <div className='secondary'>
+          <div className='recommendations'>Recommendations</div>
+        </div>
+      </div>
     </div>
   );
 }
